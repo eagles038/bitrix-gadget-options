d8cb5861f8e127bb1e7bfcec6250c717b2be2ca0
eagles038 <69106189+eagles038@users.noreply.github.com>
2023-09-19 11:14:18 +0400

    Update .description.php
    add desc

--- gadgets/custom/settings/.description.php
+++ gadgets/custom/settings/.description.php
@@ -1,1 +1,14 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arDescription = array(
+    'NAME' => 'Настройки сайта',
+    'DESCRIPTION' => 'Настройки сайта',
+    'ICON' => '',
+    'GROUP' => array('ID' => 'personal'),
+    'SU_ONLY' => false,
+    'SG_ONLY' => false,
+    'BLOG_ONLY' => false,
+    'AI_ONLY' => true,
+);
 
